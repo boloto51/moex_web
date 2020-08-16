38facf66774b71e93d11f524a216abf296477325
student-programming <student-programming@yandex.ru>
2020-08-16 16:54:49 +0300

    working delete old trades each 1 minute

--- moex_web/moex_web/Shedulers/TradeCleanerSheduler.cs
+++ moex_web/moex_web/Shedulers/TradeCleanerSheduler.cs
@@ -7,6 +7,8 @@ using moex_web.Services;
 using moex_web.Shedulers;
 using moex_web.Data.DbContext;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using moex_web.Data.Repositories;
 
 namespace moex_web.Shedulers
 {
@@ -15,36 +17,43 @@ namespace moex_web.Shedulers
         private int executionCount = 0;
         private readonly ILogger<TradeCleanerSheduler> _logger;
         private Timer _timer;
-        ISecurityTable _securityTable;
-        ITradeTable _tradeTable;
-        IDataBase _dataBase;
+        //ISecurityTable _securityTable;
+        //ITradeTable _tradeTable;
+        //IDataBase _dataBase;
+        private readonly IServiceScopeFactory _scopeFactory;
 
-        public TradeCleanerSheduler(ILogger<TradeCleanerSheduler> logger, ISecurityTable securityTable,
-            ITradeTable tradeTable, IDataBase dataBase)
+        public TradeCleanerSheduler(ILogger<TradeCleanerSheduler> logger, IServiceScopeFactory scopeFactory)
         {
             _logger = logger;
-            _securityTable = securityTable;
-            _tradeTable = tradeTable;
-            _dataBase = dataBase;
+            //_securityTable = securityTable;
+            //_tradeTable = tradeTable;
+            //_dataBase = dataBase;
+            _scopeFactory = scopeFactory;
         }
 
         public Task StartAsync(CancellationToken stoppingToken)
         {
             _logger.LogInformation("Timed Hosted Service running.");
 
-            _timer = new Timer(CleanTradeTable, null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
 
             return Task.CompletedTask;
         }
 
-        private void CleanTradeTable(object state)
+        private void DoWork(object state)
         {
-            var count = Interlocked.Increment(ref executionCount);
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var _tradeRepository = scope.ServiceProvider.GetRequiredService<ITradeRepository>();
+                var _tradeTable = scope.ServiceProvider.GetRequiredService<ITradeTable>();
 
-            _logger.LogInformation("Timed Hosted Service is working. Count: {Count}", count);
+                var count = Interlocked.Increment(ref executionCount);
 
-            string postfix_date_init = _tradeTable.ConvertDate(DateTime.Now.AddYears(-5));
-            _dataBase.DeleteOldTrades(postfix_date_init);
+                _logger.LogInformation("Timed Hosted Service is working. Count: {Count}", count);
+
+                string postfix_date_init = _tradeTable.ConvertDate(DateTime.Now.AddYears(-5));
+                _tradeRepository.DeleteOldTrades(postfix_date_init);
+            }
         }
 
         public Task StopAsync(CancellationToken stoppingToken)
--- moex_web/moex_web/Startup.cs
+++ moex_web/moex_web/Startup.cs
@@ -41,6 +41,7 @@ namespace moex_web
             services.AddScoped<IUriConverter, UriConverter>();
             services.AddScoped<IDataBase, DataBase>();
             services.AddScoped<IHttpService, HttpService>();
+            services.AddSingleton<IHostedService, TradeCleanerSheduler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
